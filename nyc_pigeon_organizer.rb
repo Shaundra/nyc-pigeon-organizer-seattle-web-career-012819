de70faa00e1beede4b5d62d24495816d3aa6dc4e
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-01-26 10:39:16 -0800

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # initialize a holder hash that creates a two-level nested hash when encountering new keys
+  new_hash = Hash.new { |hash, key| hash[key] = {} }
+
+  data.each do |attr_grp, attrs|
+    attrs.each do |attr, vals|
+      vals.each do |val|
+        if new_hash[val][attr_grp]
+          new_hash[val][attr_grp] << attr.to_s
+        else
+          new_hash[val][attr_grp] = [attr.to_s]
+        end
+      end
+    end
+  end
+
+  new_hash
 end
